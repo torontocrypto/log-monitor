770ad411cf439154db74ad111fae5080fa5540e6
saltire sable <x@saltiresable.com>
2015-05-05 00:35:08 -0400

    Fix hostname

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-var socket = io('http://localhost:9000');
+var socket = io(window.location.hostname + ':' + window.location.port);
 socket.on('test', function (data) {
     $.each(data.lines, function (i, line) {
         $('<p>').html(line).appendTo('#log');
